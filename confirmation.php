cb138c04fb0ef8f7f5c88de2f2e0e276f84b7f18
Erik-wiklund <85eriwik@skola.boras.se>
2023-09-09 11:12:03 +0200

    Add confirmation file when creating user

---
+++ confirmation.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration Confirmation</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f5f5f5;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+
+        .confirmation-container {
+            background-color: #ffffff;
+            border-radius: 5px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+            padding: 20px;
+            text-align: center;
+        }
+
+        .confirmation-icon {
+            font-size: 48px;
+            color: #4CAF50;
+        }
+
+        h1 {
+            font-size: 24px;
+            margin-top: 10px;
+        }
+
+        p {
+            font-size: 18px;
+            margin-top: 10px;
+        }
+
+        a {
+            text-decoration: none;
+            color: #4CAF50;
+            font-weight: bold;
+        }
+    </style>
+</head>
+<body>
+    <div class="confirmation-container">
+        <div class="confirmation-icon">&#10004;</div>
+        <h1>Registration Successful</h1>
+        <p>Your account has been successfully registered.</p>
+        <p><a href="index.php">Login</a> to access your account.</p>
+    </div>
+</body>
+</html>
