927f0f2dece34fde70433a4507719d51818f5a1e
Erik-wiklund <85eriwik@skola.boras.se>
2023-09-10 11:32:22 +0200

    Add modal for login, add js handle modal

--- confirmation.php
+++ confirmation.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -43,15 +44,66 @@
             text-decoration: none;
             color: #4CAF50;
             font-weight: bold;
+            cursor: pointer;
+            /* Add cursor pointer to make it obvious it's clickable */
+        }
+        /* Modal styles */
+        .modal {
+            display: none;
+            position: fixed;
+            z-index: 1;
+            left: 0;
+            top: 0;
+            width: 100%;
+            height: 100%;
+            overflow: auto;
+            background-color: rgba(0, 0, 0, 0.4);
+        }
+
+        .modal-content {
+            background-color: white;
+            margin: 15% auto;
+            padding: 20px;
+            border: 1px solid #888;
+            width: 50%;
+        }
+
+        .close {
+            float: right;
+            font-size: 28px;
+            font-weight: bold;
+            cursor: pointer;
         }
     </style>
 </head>
+
 <body>
+    <?php include "includes/login_modal.php"; ?>
     <div class="confirmation-container">
         <div class="confirmation-icon">&#10004;</div>
         <h1>Registration Successful</h1>
         <p>Your account has been successfully registered.</p>
-        <p><a href="index.php">Login</a> to access your account.</p>
+        <p><a href="index.php">Home</a> to get back to homepage</p>
+        <?php if (!isset($_SESSION['username'])) { ?>
+           <p> <a id="loginLink" style="cursor: pointer;">Login</a> to access website and forums</p>
+        <?php } ?>
     </div>
+
+    <!-- JavaScript to open the login modal -->
+    <script>
+    document.addEventListener('DOMContentLoaded', function() {
+        // Get a reference to the login link by its ID
+        var loginLink = document.getElementById('loginLink');
+        
+        // Add a click event listener to the login link
+        loginLink.addEventListener('click', function() {
+            // Trigger the login modal to display
+            var loginModal = document.getElementById('loginModal');
+            loginModal.style.display = 'block';
+        });
+    });
+</script>
+
 </body>
+
 </html>
